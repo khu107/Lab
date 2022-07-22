3d184a34cc74f284dffc7acfa83bc03772c3bc5f
Khusan <khu10735@naver.com>
2022-07-22 14:21:13 +0900

    Navlink

--- src/compononts/Navbar/Navbar.jsx
+++ src/compononts/Navbar/Navbar.jsx
@@ -1,19 +1,50 @@
 import React from "react";
 import logoimg from "../../asset/logo.png";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import "../Navbar/Navbar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Navbar() {
+  const active = {
+    fontWeight: "bold",
+    color: "black",
+  };
+
   return (
-    <>
-      <NavLink to={"/home"}>
-        <img src={logoimg}></img>
-      </NavLink>
-      <NavLink to={"/home"}>Home</NavLink>
-      <NavLink to={"/about"}>About us</NavLink>
-      <NavLink to={"/services"}>Services</NavLink>
-      <NavLink to={"/news"}>News</NavLink>
-      <NavLink to={"/contact"}>Contact</NavLink>
-    </>
+    <div className="nav">
+      <div className="logo">
+        <NavLink to={"/home"}>
+          <img src={logoimg} />
+        </NavLink>
+      </div>
+      <div className="items">
+        <div className="item">
+          <NavLink to={"/home"} style={{ textDecoration: "none" }}>
+            Home
+          </NavLink>
+        </div>
+        <div className="item">
+          <NavLink to={"/about"} style={{ textDecoration: "none" }}>
+            About us
+          </NavLink>
+        </div>
+        <div className="item">
+          <NavLink to={"/service"} style={{ textDecoration: "none" }}>
+            Services
+          </NavLink>
+        </div>
+        <div className="item">
+          <NavLink to={"/news"} style={{ textDecoration: "none" }}>
+            News
+          </NavLink>
+        </div>
+        <div className="item">
+          <NavLink to={"/contact"} style={{ textDecoration: "none" }}>
+            Contact
+          </NavLink>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/root/root.js
+++ src/root/root.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Navbar from "../compononts/Navbar/Navbar.jsx";
+import About from "../compononts/About/about.js";
+import Home from "../compononts/Home/home.js";
+import News from "../compononts/News/news.js";
+import Contact from "../compononts/Contact/contact.js";
+import Service from "../compononts/Service/service.js";
+import { Routes, Route } from "react-router-dom";
 
 export default function Root() {
   return (
     <div className="Root">
       <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
