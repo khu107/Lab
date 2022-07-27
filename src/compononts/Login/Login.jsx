1484d94f4eeb4b8ba14e28abd40b23e8b3a25bd1
Khusan <khu10735@naver.com>
2022-07-27 16:27:45 +0900

    style

--- src/compononts/Login/Login.jsx
+++ src/compononts/Login/Login.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import "../Login/style.css";
+import { Container, Col, Row, Form, Button } from "react-bootstrap";
 
 export default function Login() {
   return (
-    <div className="log">
-      <div className="logpage">
-        <label htmlFor="">Name</label>
-        <br />
-        <input type="text" name="" id="" />
-      </div>
-    </div>
+    <>
+      <Container style={{ marginTop: "200px" }}>
+        <Row>
+          <Col lg={4} md={5} sm={12}>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group className="mb-1" controlId="formBasicPassword">
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+
+              <Button variant="black" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+          <Col lg={4} md={5} sm={12}></Col>
+        </Row>
+      </Container>
+    </>
   );
 }
